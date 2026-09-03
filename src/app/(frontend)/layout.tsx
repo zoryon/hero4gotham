b8359040242f79e0fb4e2cda559506e0403fb922
zoryon <gioelespata@gmail.com>
2026-09-03 08:30:54 +0200

    chore: update site favicon

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -63,7 +63,6 @@ export default async function RootLayout({ children }: { children: React.ReactNo
     >
       <head>
         <link href="/favicon.ico" rel="icon" sizes="32x32" />
-        <link href="/favicon.svg" rel="icon" type="image/svg+xml" />
       </head>
       <body>
         <InitTheme />
