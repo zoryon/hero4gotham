a26f53d5bc6afb502f979be8257c4d34daba43ff
zoryon <gioelespata@gmail.com>
2026-09-03 15:35:51 +0200

    fix: remove nested admin shell and simplify styling

--- src/components/AdminDashboard/View.tsx
+++ src/components/AdminDashboard/View.tsx
@@ -1,12 +1,5 @@
-import { DefaultTemplate } from '@payloadcms/next/templates'
-import type { AdminViewServerProps } from 'payload'
-
 import { AdminDashboard } from './Dashboard.client'
 
-export default function AdminDashboardView(props: AdminViewServerProps) {
-  return (
-    <DefaultTemplate {...props} visibleEntities={props.initPageResult.visibleEntities}>
-      <AdminDashboard />
-    </DefaultTemplate>
-  )
+export default function AdminDashboardView() {
+  return <AdminDashboard />
 }
